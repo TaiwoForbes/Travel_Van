3462986c3297d166704dd532f7553d582f9a7573
TaiwoForbes <soyemihissa2000@gmail.com>
2023-08-06 05:17:20 +0100

    nexted routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import './index.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Vans from './pages/vans'
-import VanDetail from './pages/vanDetail'
+import Vans from './pages/vans/Vans'
+import VanDetail from './pages/vans/VanDetail'
+import DashBoard from './pages/host/DashBoard'
+import Income from './pages/host/Income'
+import Reviews from './pages/host/Reviews'
 import "./server"
+import LayOut from './component/LayOut'
 
 
 
@@ -16,22 +20,24 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <Link className='site-logo' to='/Home'>TravelVan</Link>
-          <nav>
-            <Link to='/about'>About</Link>
-            <Link to='/vans'>Vans</Link>
-          </nav>
-        </header>
+
 
 
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/vans' element={<Vans />} />
-          <Route path='/vans/:id' element={<VanDetail />} />
+          <Route element={<LayOut />}>
+            <Route path='/home' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/vans' element={<Vans />} />
+            <Route path='/vans/:id' element={<VanDetail />} />
+
+            
+            <Route path='/host' element={<DashBoard />} >
+              <Route path='/host/income' element={<Income />} />
+              <Route path='/host/reviews' element={<Reviews />} />
+            </Route>
+          </Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter >
     </>
   )
 }

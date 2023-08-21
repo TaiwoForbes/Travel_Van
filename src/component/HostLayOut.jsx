96b6b906b3e905e36bdf8c333e0253af42a05300
TaiwoForbes <soyemihissa2000@gmail.com>
2023-08-21 06:10:24 +0100

    refactor the hostdetailpage

--- src/component/HostLayOut.jsx
+++ src/component/HostLayOut.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import '../index.css';
 
 const HostLayOut = () => {
@@ -10,21 +10,22 @@ const HostLayOut = () => {
     
 }
 
+
   return (
     <div>
       <nav className='host-nav'>
-        <Link to='/host' className={(isActive) => (isActive ?activeStyles : null)}>
+        <NavLink to='/host' className={(isActive) => (isActive ?activeStyles : null)}>
           DashBoard
-        </Link>
-        <Link to='income' className={(isActive) => (isActive ?activeStyles : null)}>
+        </NavLink>
+        <NavLink to='income' className={(isActive) => (isActive ?activeStyles : null)}>
           Income
-        </Link>
-        <Link to='vans' className={(isActive) => (isActive ?activeStyles : null)}>
+        </NavLink>
+        <NavLink to='vans' className={(isActive) => (isActive ?activeStyles : null)}>
           Vans
-        </Link>
-        <Link to='reviews' className={(isActive) => (isActive ?activeStyles : null)}>
+        </NavLink>
+        <NavLink to='reviews' className={(isActive) => (isActive ?activeStyles : null)}>
           Reviews
-        </Link>
+        </NavLink>
       </nav>
       <Outlet />
     </div>
--- src/pages/host/HostVanDetails.jsx
+++ src/pages/host/HostVanDetails.jsx
@@ -1,39 +1,48 @@
-import React, { useState, useEffect } from "react"
-import { useParams, Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-export default function HostVanDetail() {
-    const { id } = useParams()
-    const [currentVan, setCurrentVan] = useState(null)
-
-    useEffect(() => {
-        fetch(`/api/host/vans/${id}`)
-            .then(res => res.json())
-            .then(data => setCurrentVan(data.vans[0]))
-    }, [])
-   
-
+function VanDetailSection({ van }) {
     return (
-
-        <section>
-            {currentVan ? (
-            <div className="host-van-detail-layout-container">
-                <div className="host-van-detail">
-                    <img src={currentVan.imageUrl} />
-                    <div className="host-van-detail-info-text">
-                        <i
-                            className={`van-type van-type-${currentVan.type}`}
-                        >
-                            {currentVan.type}
-                        </i>
-                        <h3>{currentVan.name}</h3>
-                        <h4>${currentVan.price}/day</h4>
-                    </div>
+        <div className="host-van-detail-layout-container">
+            <div className="host-van-detail">
+                <img src={van.imageUrl} alt={van.name} />
+                <div className="host-van-detail-info-text">
+                    <i className={`van-type van-type-${van.type}`}>
+                        {van.type}
+                    </i>
+                    <h3>{van.name}</h3>
+                    <h4>${van.price}/day</h4>
                 </div>
             </div>
-            ) : (
-            <h1>Loading...</h1>
-            )}
+        </div>
+    );
+}
 
+export default function HostVanDetail() {
+    const { id } = useParams();
+    const [currentVan, setCurrentVan] = useState(null);
+
+    useEffect(() => {
+        const fetchVanData = async () => {
+            try {
+                const response = await fetch(`/api/host/vans/${id}`);
+                const data = await response.json();
+                setCurrentVan(data.vans[0]);
+            } catch (error) {
+                console.error("Error fetching van data:", error);
+            }
+        };
+
+        fetchVanData();
+    }, [id]);
+
+    return (
+        <section>
+            {currentVan ? (
+                <VanDetailSection van={currentVan} />
+            ) : (
+                <h1>Loading...</h1>
+            )}
         </section>
-    )
+    );
 }
